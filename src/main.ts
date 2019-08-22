df593065d2807c21bf9d7cc7d9d020b430f6b2f8
Yi Lu <luyi985@gmail.com>
2019-08-22 23:10:04 +1000

    setup complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
-console.log('running --ds')
+import dotenv from 'dotenv';
+dotenv.config();
+import { ApolloServer } from 'apollo-server';
+import resolvers from './resolvers';
+import typeDefs from './schemas';
+import { environment } from './environment';
+
+const server = new ApolloServer({
+	resolvers,
+	typeDefs,
+	...environment.apollo,
+});
+
+server.listen(environment.port).then(({ url }) => console.log(`Server ready at ${url}. `));
 
 // Hot Module Replacement
 if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => console.log('Module disposed. '));
+	module.hot.accept();
+	module.hot.dispose(() => console.log('Module disposed. '));
 }
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,4 @@
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const merge = require('webpack-merge');
 const nodeExternals = require('webpack-node-externals');
 const path = require('path');
@@ -7,14 +7,14 @@ const webpack = require('webpack');
 const common = require('./webpack.common.js');
 
 module.exports = merge.smart(common, {
-  devtool: 'inline-source-map',
-  entry: ['webpack/hot/poll?1000', path.join(__dirname, 'src/main.ts')],
-  externals: [
-    nodeExternals({
-      whitelist: ['webpack/hot/poll?1000']
-    })
-  ],
-  mode: 'development',
-  plugins: [new CleanWebpackPlugin(), new webpack.HotModuleReplacementPlugin()],
-  watch: true
+	devtool: 'inline-source-map',
+	entry: ['webpack/hot/poll?1000', path.join(__dirname, 'src/main.ts')],
+	externals: [
+		nodeExternals({
+			whitelist: ['webpack/hot/poll?1000'],
+		}),
+	],
+	mode: 'development',
+	plugins: [new CleanWebpackPlugin(), new webpack.HotModuleReplacementPlugin()],
+	watch: false,
 });
